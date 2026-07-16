e8600c382859acdc29b68c1c173db1a7fb52746b
Abdulfetah Shehab <116856656+abdulfetahshehab@users.noreply.github.com>
2026-07-16 09:32:09 -0400

    Add files via upload

--- asset/layout.js
+++ asset/layout.js
@@ -1,17 +1,23 @@
 
 
 
-let path = window.location.pathname
+let path = window.location.pathname.includes("/pages/")
     ? "../component/"
     : "component/";
 
 
 // Navbar
 fetch(path + "navbar.html")
-.then(response => response.text())
+.then(response => {
+    if (!response.ok){
+        throw new Error("Navbar not founde");
+    }
+    return response.text();
+})
 .then(data => {
     document.getElementById("navbar").innerHTML = data;
-});
+})
+.catch(error => console.log(error));
 
 
 // Sidebar
